057d196fef15fcacb4ac0b9c20f3b39c1109385e
ChaseTheHuman <ChaseARichardson2003@gmail.com>
2022-03-27 01:37:38 -0500

    Add files via upload

---
+++ ExtensionSorter.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.*;
+import java.util.*;
+
+public class ExtensionSorter {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Chase\\Downloads\\SortPractice";
+        File file = new File(path);
+        File downloadDir[] = file.listFiles();
+        Arrays.sort(downloadDir);
+        for (File e : downloadDir) {
+            if (e.isFile()) {
+                // String last = e.getAbsolutePath().lastIndexOf(",")
+                String[] temp = e.getAbsolutePath().split("\\.(?=[^.]*$)", 2);
+                System.out.println("this is full " + e.getAbsolutePath());
+                // System.out.println("this is 1 " + temp[0]);
+                // System.out.println("this is 2 " + temp[1]);
+                File f = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length - 1]);
+
+                if (f.mkdir()) {
+                    System.out.println("Directory is created");
+                    try {
+                        File fileLoc = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length-1] + "\\"+ e.getName());
+                        Files.move(e.toPath(), fileLoc.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("file move successful");
+                    } catch (IOException p) {
+                        System.out.println("not successful");
+                        p.printStackTrace();
+                    }
+                } else {
+                    System.out.println("Directory cannot be created");
+                    try {
+                        File fileLoc = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length-1] + "\\"+ e.getName());
+                        Files.move(e.toPath(), fileLoc.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("file move successful");
+                    } catch (IOException p) {
+                        System.out.println("not successful");
+                        p.printStackTrace();
+                    }
+                }
+            }
+            // Consider directory sort, although would be stupid
+
+        }
+    }
+}
