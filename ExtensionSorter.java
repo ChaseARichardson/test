d8c11910f513f7deecf43fd316170c7137431cf8
Sam Vader <samvader13@gmail.com>
2022-03-27 01:45:18 -0500

    add message

--- ExtensionSorter.java
+++ ExtensionSorter.java
@@ -4,9 +4,11 @@ import java.nio.file.Files;
 import java.nio.file.*;
 import java.util.*;
 
+// YOOOOO WHAT's UP CHASE
+
 public class ExtensionSorter {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Chase\\Downloads\\SortPractice";
+        String path = "C:\\Users\\samva\\Downloads\\";
         File file = new File(path);
         File downloadDir[] = file.listFiles();
         Arrays.sort(downloadDir);
@@ -17,12 +19,12 @@ public class ExtensionSorter {
                 System.out.println("this is full " + e.getAbsolutePath());
                 // System.out.println("this is 1 " + temp[0]);
                 // System.out.println("this is 2 " + temp[1]);
-                File f = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length - 1]);
+                File f = new File("C:\\Users\\samva\\Downloads\\" + temp[temp.length - 1]);
 
                 if (f.mkdir()) {
                     System.out.println("Directory is created");
                     try {
-                        File fileLoc = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length-1] + "\\"+ e.getName());
+                        File fileLoc = new File("C:\\Users\\samva\\Downloads\\" + temp[temp.length-1] + "\\"+ e.getName());
                         Files.move(e.toPath(), fileLoc.toPath(), StandardCopyOption.REPLACE_EXISTING);
                         System.out.println("file move successful");
                     } catch (IOException p) {
@@ -32,7 +34,7 @@ public class ExtensionSorter {
                 } else {
                     System.out.println("Directory cannot be created");
                     try {
-                        File fileLoc = new File("C:\\Users\\Chase\\Downloads\\SortPractice\\" + temp[temp.length-1] + "\\"+ e.getName());
+                        File fileLoc = new File("C:\\Users\\samva\\Downloads\\" + temp[temp.length-1] + "\\"+ e.getName());
                         Files.move(e.toPath(), fileLoc.toPath(), StandardCopyOption.REPLACE_EXISTING);
                         System.out.println("file move successful");
                     } catch (IOException p) {
